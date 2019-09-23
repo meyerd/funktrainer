08afc02f08f261b83d6f2e4335f27ebe54b701e7
Dominik <meyerd@mytum.de>
2019-09-23 13:27:14 +0200

    add one basic test

---
+++ app/src/test/java/testBasicTestFrameworkSetup.java
@@ -0,0 +1,10 @@
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class testBasicTestFrameworkSetup extends TestCase {
+
+    @Test
+    public void testBasicTestFrameworkSetup_testTestsRunning() {
+        assertEquals(true, !false);
+    }
+}
